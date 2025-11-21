d23d33b5dba2e65da7c45feef13f794b33363ee4
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-11-21 19:02:25 +0600

    added Brands slider

---
+++ src/pages/Home/Brands/Brands.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+// 1. Import Swiper and SwiperSlide from 'swiper/react'
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import amazon from "../../../assets/brands/amazon.png";
+import casio from "../../../assets/brands/casio.png";
+import moonstar from "../../../assets/brands/moonstar.png";
+import randstad from "../../../assets/brands/randstad.png";
+import star from "../../../assets/brands/star.png";
+import start_people from "../../../assets/brands/start_people.png";
+import amazon_vector from "../../../assets/brands/amazon_vector.png";
+
+const brandsLogo = [
+  amazon,
+  casio,
+  moonstar,
+  randstad,
+  star,
+  start_people,
+  amazon_vector,
+];
+
+const Brands = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-center font-semibold pb-10 text-2xl text-secondary">We've helped thousands of sales teams</h1>
+      </div>
+      <Swiper
+        slidesPerView={4}
+        loop={true}
+        centeredSlides={true}
+        grabCursor={true}
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
+        className="mySwiper"
+      >
+        {brandsLogo.map((logo, index) => (
+          <SwiperSlide key={index}>
+            <img src={logo} alt={`Brand ${index}`}  />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Brands;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../Hero/Hero";
 import HowWorks from "../How it Works/HowWorks";
 import OurServices from "../Our Services/OurServices";
+import Brands from "../Brands/Brands";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <div className="my-15"><HowWorks /></div>
       <div className="py-8 bg-secondary rounded-2xl"><OurServices /></div>
+      <div className="my-15"><Brands /></div>
     </div>
   );
 };
